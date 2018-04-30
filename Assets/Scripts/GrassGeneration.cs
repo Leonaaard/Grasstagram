8e2f28a9ab1a954ae85111ca744b1de06d06c30f
leonaaard <contact.leo.faure@gmail.com>
2018-04-30 15:18:43 +0200

    v1

--- Assets/Scripts/GrassGeneration.cs
+++ Assets/Scripts/GrassGeneration.cs
@@ -4,29 +4,32 @@ using UnityEngine;
 
 public class GrassGeneration : MonoBehaviour {
 
-    public GameObject gameManager;
-
     public GameObject plant;
 
+    public float plantHeight;
+    public float plantHeightRandomisation;
+
     public float plantQuantity;
+    public float grassSize;
 
 	IEnumerator Start () {
 
         // Référence au script GameManager
+        GameObject gameManager = GameObject.FindWithTag("GameManager");
         GameManager gameManagerScript = gameManager.GetComponent<GameManager>();
 
         for (int i = 0; i < plantQuantity; i++)
         {
 
-            // Position de la plante
+            // Position de la plante + surface de l'herbe
             Vector3 instancePosition = new Vector3(
-                transform.position.x + i,
+                transform.position.x + Random.Range(-grassSize, grassSize),
                 transform.position.y,
-                transform.position.z
-                );
+                transform.position.x + Random.Range(-grassSize, grassSize)
+            );
 
             // Création de la plante
-            GameObject instancePlant = Instantiate(plant, transform.position, Quaternion.identity);
+            GameObject instancePlant = Instantiate(plant, instancePosition, Quaternion.identity);
             instancePlant.transform.SetParent(transform);
 
 
--- Assets/Scripts/PlantGeneration.cs
+++ Assets/Scripts/PlantGeneration.cs
@@ -4,20 +4,19 @@ using UnityEngine;
 
 public class PlantGeneration : MonoBehaviour {
 
-    public GameObject gameManager;
-
     public GameObject leaf;
-
-    public float plantHeight;
-
 
 	IEnumerator Start () {
 
         // Référence au script GameManager
+        GameObject gameManager = GameObject.FindWithTag("GameManager");
         GameManager gameManagerScript = gameManager.GetComponent<GameManager>();
 
+        // Référence au script Parent
+        GrassGeneration parent = transform.GetComponentInParent<GrassGeneration>();
+
         // Taille de la plante aléatoire
-        float plantRandomHeight = plantHeight + Random.Range(0, 60);
+        float plantRandomHeight = parent.plantHeight + parent.plantHeightRandomisation;
 
         // Couleur de la plante aléatoire
         Color leafColor = new Color(0f, Random.Range(0.4f, 0.7f), Random.Range(0f, 0.3f), 1f);
@@ -28,13 +27,23 @@ public class PlantGeneration : MonoBehaviour {
             // Décalage vertical entre chaque feuille
             Vector3 instancePosition = new Vector3(
                 transform.position.x,
-                transform.position.y + (i/10),
+                transform.position.y + (i*  0.1f) + 0.01f,
                 transform.position.z
+                );
+
+            // Réduction de la taille à chaque feuille
+            Vector3 instanceSize = new Vector3(
+                1 - (i * 0.01f),
+                1 - (i * 0.01f),
+                1
                 );
 
             // Création de la feuille
             GameObject instanceLeaf = Instantiate(leaf, instancePosition, Quaternion.Euler(-90,0,0));
             instanceLeaf.transform.SetParent(transform);
+
+            // Taille de la feuille
+            instanceLeaf.transform.localScale = instanceSize;
 
             // Colorisation de la feuille
             instanceLeaf.GetComponent<SpriteRenderer>().color = leafColor;
