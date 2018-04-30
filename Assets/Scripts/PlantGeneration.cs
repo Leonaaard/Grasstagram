6ae59d060de87460c3744c8a24d93b76c5628110
leonaaard <contact.leo.faure@gmail.com>
2018-04-30 14:17:59 +0200

    debut

---
+++ Assets/Scripts/PlantGeneration.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantGeneration : MonoBehaviour {
+
+    public GameObject gameManager;
+
+    public GameObject leaf;
+
+    public float plantHeight;
+
+
+	IEnumerator Start () {
+
+        GameManager gameManagerScript = gameManager.GetComponent<GameManager>();
+
+        for (int i = 0; i < plantHeight; i++)
+        {
+
+            GameObject instanceLeaf = Instantiate(leaf, new Vector3(0, 0, 0), Quaternion.Euler(-90,0,0));
+            instanceLeaf.transform.SetParent(transform);
+
+            yield return new WaitForSeconds(gameManagerScript.generationSpeed);
+
+        }
+
+	}
+	
+	void Update () {
+		
+	}
+}
